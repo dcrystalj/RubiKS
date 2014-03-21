11d36c8ae28049e8b15d1444c087c9605854d890
Uroš Hekić <uros.hekic@gmail.com>
2014-03-21 14:41:33 +0100

    Fixed date

--- rubiKS/app/views/algorithms/show.blade.php
+++ rubiKS/app/views/algorithms/show.blade.php
@@ -1,7 +1,7 @@
 @extends('main')
 @section('content')
 	<h4>Algoritmi</h4>
-	<h3>{{ $competition->name }} <small>{{ Help::date($competition->date) }}</small></h3>
+	<h3>{{ $competition->name }} <small>{{ Date::parse($competition->date) }}</small></h3>
 	@if(count($contents) > 0)
 		@foreach ($contents as $f)
 			<a href="{{ asset($path . '/' . $competition->short_name . '/' . $f) }}">{{ $f }}</a>
