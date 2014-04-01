520a0fdfa75a05e85207a1d76254b8a1003f3b58
Uroš Hekić <uros.hekic@gmail.com>
2014-04-01 19:09:25 +0200

    Link to competitor's page (Registrations)

--- rubiKS/app/views/competitions/registrations.blade.php
+++ rubiKS/app/views/competitions/registrations.blade.php
@@ -17,7 +17,7 @@
 			<?php $competitor = $registration->user; $userEvents = 0; ?>
 			<tr>
 				<td class="text-right"><small>{{ $i + 1 }}.</small></td>
-				<td><small>{{ $competitor->getFullName() }}</small></td>
+				<td><small><a href="{{ url('competitors', $competitor->club_id) }}">{{ $competitor->getFullName() }}</a></small></td>
 				@foreach ($events as $event)
 					<td><small>@if ($registration->signedUpForEvent($event->readable_id)) X <?php $userEvents++; ?>@else - @endif</small></td>
 				@endforeach
