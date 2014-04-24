9dab13b899ab6e46fa12973e3300eba17e519e2d
Uroš Hekić <uros.hekic@gmail.com>
2014-04-24 21:28:30 +0200

    News - title link

--- rubiKS/app/views/news/index.blade.php
+++ rubiKS/app/views/news/index.blade.php
@@ -3,7 +3,7 @@
 	<h4>Novice</h4>
 	@foreach ($news as $article)
 		<span class="news">
-			<h2>{{ $article->title }} <small class="pull-right"><a href="{{ route('news.show', $article->url_slug) }}">#</a> {{ $article->getParsedDateShort() }}</small></h2>
+			<h2><a href="{{ route('news.show', $article->url_slug) }}">{{ $article->title }}</a> <small class="pull-right"><a href="{{ route('news.show', $article->url_slug) }}">#</a> {{ $article->getParsedDateShort() }}</small></h2>
 			<p>{{ $article->text }}</p>
 		</span>
 		<hr>
