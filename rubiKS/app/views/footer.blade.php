0c2136dc6980463739131fd473c9a2698cf98779
Uroš Hekić <uros.hekic@gmail.com>
2014-05-25 16:07:15 +0200

    Templates - responsive footer

--- rubiKS/app/views/footer.blade.php
+++ rubiKS/app/views/footer.blade.php
@@ -1,25 +1,26 @@
 <div class="footer">
   <div class="container">
-  	<div class="footer_block">
+  	<div class="footer_block col-xs-6 col-sm-4 col-md-3">
   		<img alt="RubiKS dejavnosti - pridružite se nam!" src="{{ URL::asset('assets/img/parola.png') }}" width="150">
   	</div>
-  	<div class="footer_block">
+  	<div class="footer_block col-xs-6 col-sm-8 col-md-3">
     	<b>Rubik klub Slovenija</b> <br>
     	© Vse pravice pridržane
+      <br><br><br>{{-- dirty trick :) fix this! --}}
     </div>
-  	<div class="footer_block">
+  	<div class="footer_block col-xs-4 col-md-2">
   		<a href="{{ route('competitions.index') }}">Tekmovanja</a> <br>
       <a href="{{ route('competitors.index') }}">Tekmovalci</a> <br>
       <a href="{{ route('rankings.index') }}">Rezultati</a> <br>
       <a href="{{ url('records') }}">Rekordi</a> <br>
   	</div>
-    <div class="footer_block">
+    <div class="footer_block col-xs-4 col-md-2">
     	<a href="{{ url('https://picasaweb.google.com/100258962713596029232') }}" target="_blank">Foto</a> <br>
     	<a href="{{ url('https://www.youtube.com/user/RubikKlubSlovenija') }}" target="_blank">Video</a> <br>
     	<a href="{{ url('http://www.rubik.si/') }}">Forum</a> <br>
       <a href="{{ route('news.index') }}">Arhiv novic</a> <br>
     </div>
-    <div class="footer_block">
+    <div class="footer_block col-xs-4 col-md-2">
       <a href="{{ url('static/o-klubu') }}">O klubu</a> <br>
       <a href="{{ url('static/vclanite-se-v-klub') }}">Članstvo</a> <br>
       <a href="{{ url('static/pravilniki') }}">Pravila</a> <br>
