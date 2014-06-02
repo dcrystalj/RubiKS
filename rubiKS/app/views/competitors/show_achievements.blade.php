b5fa35f06653d743c8f8a4d8f15c74aec3737ba2
Uroš Hekić <uros.hekic@gmail.com>
2014-06-02 23:34:24 +0200

    Templates - Competitor (achievements page)

--- rubiKS/app/views/competitors/show_achievements.blade.php
+++ rubiKS/app/views/competitors/show_achievements.blade.php
@@ -5,8 +5,22 @@
 	@foreach ($finalMedals as $medal)
 		<b>{{ Help::medal($medal->rank) }} {{ $medal->rank }}. mesto</b> v skupnem seštevku <b>DP {{ $medal->year }}</b> <br>
 	@endforeach
+
 	@if (count($finalMedals) > 0 && count($eventMedals) > 0) <hr> @endif
+
+	<?php
+		$years = array();
+		$first = True;
+	?>
 	@foreach ($eventMedals as $medal)
+		@if (!in_array($medal->year, $years))
+			<?php
+				$years[] = $medal->year;
+				$newYear = True;
+				if (!$first) { echo '<br>'; } else { $first = False; }
+				?>
+			<b>{{ $medal->year }}</b> <br>
+		@endif
 		<b>{{ Help::medal($medal->rank) }} {{ $medal->rank }}. mesto</b> v disciplini <b>{{ $medal->event->name }}</b> na <b>DP {{ $medal->year }}</b> <br>
 	@endforeach
 	</div>
