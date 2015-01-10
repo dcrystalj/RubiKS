26e2a1baa5163acc1d185ad36106da37e3a7a716
Uroš Hekić <uros.hekic@gmail.com>
2015-01-10 10:56:45 +0100

    Bugfix/typo

--- rubiKS/app/views/algorithms/index.blade.php
+++ rubiKS/app/views/algorithms/index.blade.php
@@ -2,6 +2,6 @@
 @section('content')
 	<h4>Arhiv algoritmov</h4>
 	@foreach ($contents as $f)
-		<a href="{{ routes('algorithms.show', $f) }}">{{ $f }}</a> <br>
+		<a href="{{ route('algorithms.show', $f) }}">{{ $f }}</a> <br>
 	@endforeach
 @stop
