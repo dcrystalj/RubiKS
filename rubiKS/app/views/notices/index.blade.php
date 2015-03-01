387630a8092d3670fd4100aa7f4d8e7c471cb0f8
Uroš Hekić <uros.hekic@gmail.com>
2015-03-01 09:52:28 +0100

    News & notices justify text align

--- rubiKS/app/views/notices/index.blade.php
+++ rubiKS/app/views/notices/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 	<h4>Obvestila</h4>
 	@foreach ($notices as $notice)
-		<span class="news">
+		<span class="news-article">
 			<h2>{{ $notice->title }} <small class="pull-right">{{ Date::dateTime($notice->created_at, TRUE) }}</small></h2>
 			<p>{{ $notice->text }}</p>
 			<p class="text-right"><small>Veljavno do: {{ Date::parse($notice->visible_until) }}</small></p>
