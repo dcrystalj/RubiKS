b2b548919a11b5851cff12bae966e674e5ec80d0
Uroš Hekić <uros.hekic@gmail.com>
2015-03-02 22:38:41 +0100

    Static pages (justify)

--- rubiKS/app/views/staticpages/show.blade.php
+++ rubiKS/app/views/staticpages/show.blade.php
@@ -1,7 +1,9 @@
 @extends('main')
 @section('content')
 	<h4>{{ $page->title }}</h4>
-	{{ $page->text }}
+	<span class="news-article">
+        {{ $page->text }}
+    </span>
 	<br>
 	<div class="text-right"><small>Nazadnje posodobljeno: {{ Date::dateTime($page->updated_at, TRUE) }}</small></div>
 @stop
